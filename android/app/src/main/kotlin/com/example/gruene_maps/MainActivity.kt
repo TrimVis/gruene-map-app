d88c0f639d43173ce1d9ca192b968953046be3e2
Pasquale Jordan <jordanpa@ethz.ch>
2024-04-02 16:22:27 +0200

    Initial push

---
+++ android/app/src/main/kotlin/com/example/gruene_maps/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gruene_maps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/map.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class MyMapPage extends StatefulWidget {
+  const MyMapPage({super.key});
+
+  @override
+  State<MyMapPage> createState() => _MyMapPageState();
+}
+
+class _MyMapPageState extends State<MyMapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: const MapOptions(
+        initialCenter: LatLng(51.509364, -0.128928),
+        initialZoom: 9.2,
+      ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+        RichAttributionWidget(
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () =>
+                  launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
